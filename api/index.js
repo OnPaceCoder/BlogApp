10c28f6e17f5deaf5bb5429cda4557bedae50d7e
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-06-20 20:03:39 +0530

    User Created in backend

--- api/index.js
+++ api/index.js
@@ -1,12 +1,27 @@
 const express = require("express")
 const cors = require('cors')
+const mongoose = require("mongoose")
+require('dotenv').config()
+const User = require("./models/User")
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.post("/register", (req, res) => {
+
+mongoose.connect(`mongodb+srv://priyanksoftcolon:${process.env.PASSWORD}@cluster0.17c4qiy.mongodb.net/`)
+
+
+
+app.post("/register", async (req, res) => {
     const { username, password } = req.body;
-    res.json({ requestData: { username, password } })
+    const userDoc = await User.create({
+        username, password
+    });
+    res.json(userDoc)
 })
 
+
+
+
+// mongodb+srv://priyanksoftcolon:<password>@cluster0.17c4qiy.mongodb.net/
 app.listen(4000)
